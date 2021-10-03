754f715f2f48a67dc8cca3b94bdcd0f85cd87765
saumj <91833161+saumj@users.noreply.github.com>
2021-10-03 20:31:50 +0530

    Update Factorial_java.java

--- Factorial_java.java
+++ Factorial_java.java
@@ -5,6 +5,6 @@ class FactorialExample{
   for(i=1;i<=number;i++){    
       fact=fact*i;    
   }    
-  System.out.println("Factorial of "+number+" is: "+fact);    
+  System.out.println("Factorial is: "+fact);    
  }  
 }  
